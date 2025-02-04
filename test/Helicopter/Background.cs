88af8b117b313fbfdb75201e7709d6e0b707489c
Shadow <85422109+shadowartist201@users.noreply.github.com>
2025-02-04 13:02:36 -0500

    Commenting bg layers

--- test/Helicopter/Background.cs
+++ test/Helicopter/Background.cs
@@ -39,7 +39,7 @@ namespace Helicopter
             this.midPart_.Update(dt);
 		}
 
-		public void DrawBackBack(SpriteBatch spriteBatch)
+		public void DrawBackBack(SpriteBatch spriteBatch) //layer 0
 		{
 			if (this.backPartStaticLayer_ == 0)
 			{
@@ -48,7 +48,7 @@ namespace Helicopter
 			this.backParts_[0].Draw(spriteBatch);
 		}
 
-		public void DrawMiddleBack(SpriteBatch spriteBatch)
+		public void DrawMiddleBack(SpriteBatch spriteBatch) //layer 1
 		{
 			if (this.backParts_.Length > 1)
 			{
@@ -60,7 +60,7 @@ namespace Helicopter
 			}
 		}
 
-		public void DrawBack(SpriteBatch spriteBatch)
+		public void DrawBack(SpriteBatch spriteBatch) //layer 2
 		{
 			for (int i = 2; i < this.backParts_.Length; i++)
 			{
@@ -76,12 +76,12 @@ namespace Helicopter
 			}
 		}
 
-		public void DrawMiddle(SpriteBatch spriteBatch)
+		public void DrawMiddle(SpriteBatch spriteBatch) //outmost layer
 		{
 			this.midPart_.Draw(spriteBatch);
 		}
 
-		public void DrawFore(SpriteBatch spriteBatch)
+		public void DrawFore(SpriteBatch spriteBatch) //unused?
 		{
 		}
 
@@ -110,64 +110,64 @@ namespace Helicopter
 			{
 				this.backPartStaticLayer_ = 4;
 				Global.BGTexture = this.Content.Load<Texture2D>("SeaBGXbox");
-				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(4096f, 514f), Vector2.Zero, TexDirection.RIGHT, 100f);
+				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(4096f, 514f), Vector2.Zero, TexDirection.RIGHT, 100f); //sky
 				for (int i = 1; i < this.backParts_.Length; i++)
 				{
-					this.backParts_[i].Reset(Global.BGTexture, Vector2.Zero, Vector2.Zero, Vector2.Zero, TexDirection.RIGHT, 0f);
+					this.backParts_[i].Reset(Global.BGTexture, Vector2.Zero, Vector2.Zero, Vector2.Zero, TexDirection.RIGHT, 0f); //null
 				}
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 514f), new Vector2(2560f, 720f), Vector2.Zero, TexDirection.RIGHT, 200f);
-				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(2560, 514, 1280, 720), Vector2.Zero);
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 514f), new Vector2(2560f, 720f), Vector2.Zero, TexDirection.RIGHT, 200f); //mountains
+				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(2560, 514, 1280, 720), Vector2.Zero); //sun
 				break;
 			}
 			case 1:
 				this.backPartStaticLayer_ = 2;
 				Global.BGTexture = this.Content.Load<Texture2D>("cloudBGXbox");
-				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(2560f, 627f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f);
-				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 627f), new Vector2(2560f, 437f), new Vector2(0f, 283f), TexDirection.RIGHT, 100f);
-				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 1762, 1077, 559), new Vector2(348f, 0f));
-				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 1064f), new Vector2(2560f, 485f), new Vector2(0f, 235f), TexDirection.RIGHT, 150f);
-				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f);
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1549f), new Vector2(2560f, 213f), new Vector2(0f, 507f), TexDirection.RIGHT, 300f);
+				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(2560f, 627f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f); //sky
+				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 627f), new Vector2(2560f, 437f), new Vector2(0f, 283f), TexDirection.RIGHT, 100f); //cloud row 1
+				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 1762, 1077, 559), new Vector2(348f, 0f)); //sun
+				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 1064f), new Vector2(2560f, 485f), new Vector2(0f, 235f), TexDirection.RIGHT, 150f); //cloud row 2
+				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f); //null
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1549f), new Vector2(2560f, 213f), new Vector2(0f, 507f), TexDirection.RIGHT, 300f); //cloud row 3
 				break;
 			case 2:
 				this.backPartStaticLayer_ = 4;
 				Global.BGTexture = this.Content.Load<Texture2D>("lavaBGXbox");
-				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(4096f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f);
-				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(4096f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 150f);
-				this.backParts_[2].Reset(Global.BGTexture, Vector2.Zero, Vector2.Zero, Vector2.Zero, TexDirection.RIGHT, 0f);
-				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f);
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1440f), new Vector2(4096f, 92f), new Vector2(0f, 628f), TexDirection.RIGHT, 300f);
-				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f));
+				this.backParts_[0].Reset(Global.BGTexture, Vector2.Zero, new Vector2(4096f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f); //lava wall 
+				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(4096f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 150f); //cave
+				this.backParts_[2].Reset(Global.BGTexture, Vector2.Zero, Vector2.Zero, Vector2.Zero, TexDirection.RIGHT, 0f); //null
+				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f); //null
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1440f), new Vector2(4096f, 92f), new Vector2(0f, 628f), TexDirection.RIGHT, 300f); //rock
+				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f)); //null
 				break;
 			case 3:
 				this.backPartStaticLayer_ = -1;
 				Global.BGTexture = this.Content.Load<Texture2D>("meatBGXbox");
-				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2560f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 75f);
-				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 1329f), new Vector2(2560f, 258f), new Vector2(0f, 272f), TexDirection.RIGHT, 150f);
-				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(2560f, 457f), new Vector2(0f, 263f), TexDirection.RIGHT, 175f);
-				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f);
-				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f));
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1177f), new Vector2(2560f, 152f), new Vector2(0f, 598f), TexDirection.RIGHT, 200f);
+				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2560f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 75f); //sky
+				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 1329f), new Vector2(2560f, 258f), new Vector2(0f, 272f), TexDirection.RIGHT, 150f); //rocks row 3
+				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(2560f, 457f), new Vector2(0f, 263f), TexDirection.RIGHT, 175f); //meat row 1
+				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f); //null
+				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f)); //null
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 1177f), new Vector2(2560f, 152f), new Vector2(0f, 598f), TexDirection.RIGHT, 200f); //bacon row 2
 				break;
 			case 4:
 				this.backPartStaticLayer_ = 2;
 				Global.BGTexture = this.Content.Load<Texture2D>("ronBGXbox");
-				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2560f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 75f);
-				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(2560f, 317f), new Vector2(0f, 403f), TexDirection.RIGHT, 150f);
-				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 1040f), new Vector2(2560f, 350f), new Vector2(0f, 370f), TexDirection.RIGHT, 175f);
-				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 1390f), new Vector2(2560f, 254f), new Vector2(0f, 466f), TexDirection.RIGHT, 300f);
-				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f));
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f);
+				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2560f, 720f), new Vector2(0f, 0f), TexDirection.RIGHT, 75f); //sky
+				this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 720f), new Vector2(2560f, 317f), new Vector2(0f, 403f), TexDirection.RIGHT, 150f); //hills row 1
+				this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 1040f), new Vector2(2560f, 350f), new Vector2(0f, 370f), TexDirection.RIGHT, 175f); //hills row 2
+				this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 1390f), new Vector2(2560f, 254f), new Vector2(0f, 466f), TexDirection.RIGHT, 300f); //hills row 3
+				this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f)); //null
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f); //null
 				break;
 			case 5:
 				this.backPartStaticLayer_ = 2;
 				Global.BGTexture = this.Content.Load<Texture2D>("nyanBGios");
-				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2259f, 627f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f); //bg
-                this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 627f), new Vector2(2259f, 237f), new Vector2(0f, 379f), TexDirection.RIGHT, 150f); //hills
-                this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 864f), new Vector2(2259f, 184f), new Vector2(0f, 536f), TexDirection.RIGHT, 200f); //grass
-                this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f);
-                this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f));
-				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f);
+				this.backParts_[0].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(2259f, 627f), new Vector2(0f, 0f), TexDirection.RIGHT, 50f); //sky
+                this.backParts_[1].Reset(Global.BGTexture, new Vector2(0f, 627f), new Vector2(2259f, 237f), new Vector2(0f, 379f), TexDirection.RIGHT, 150f); //hills row 1
+                this.backParts_[2].Reset(Global.BGTexture, new Vector2(0f, 864f), new Vector2(2259f, 184f), new Vector2(0f, 536f), TexDirection.RIGHT, 200f); //grass row 2
+                this.backParts_[3].Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 0f); //null
+                this.backPartStatic_.Reset(Global.BGTexture, new Rectangle(0, 0, 0, 0), new Vector2(0f, 0f)); //null
+				this.midPart_.Reset(Global.BGTexture, new Vector2(0f, 0f), new Vector2(0f, 0f), new Vector2(0f, 0f), TexDirection.RIGHT, 300f); //null
                 break;
 			}
 		}
